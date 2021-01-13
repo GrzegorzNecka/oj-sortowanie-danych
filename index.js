e120385237ab6fc5203820909f77e0d3fa0ddbd4
GrzegorzNecka <strzypior@gmail.com>
2021-01-13 11:20:10 +0100

    1 file modified

--- index.js
+++ index.js
@@ -3,15 +3,36 @@
 const table = document.querySelector(".table-auto");
 const trs = table.querySelectorAll("tbody tr");
 const ths = table.querySelectorAll("thead th");
+const makeArr = htmlElem => Array.from(htmlElem);
 
 function sortBy(e) {
-  console.dir(e.target);
+  console.dir("target", e.target);
+  const index = e.target.cellIndex;
   // console.dir(e.target.cellIndex);
-  let anc = Array.from(ths);
+  let anc = makeArr(ths);
   console.log(anc);
+
   console.log(anc.indexOf(e.target));
+
+  //============================
+
+  makeArr(trs).sort((a, b) => {
+    const tdA = a.children[index];
+    const tdB = b.children[index];
+    // console.log(tdA);
+    // console.log(tdB);
+  });
+
+  //test
+  makeArr(trs).forEach(a => {
+    const tdA = a.children[index];
+
+    console.log(tdA);
+  });
 }
 
+//13:15 -- https://eduweb.pl/programowanie-i-www/javascript/javascript-podstawy/sortowanie-tabeli-po-kolumnach-cz.-1
+//============================
 for (let i = 0; i < ths.length; i++) {
   ths[i].addEventListener("click", sortBy, true);
 }
