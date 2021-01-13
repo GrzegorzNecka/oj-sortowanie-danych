6b8ae8621378dd9800feb32924e871993af05171
GrzegorzNecka <strzypior@gmail.com>
2021-01-13 10:44:43 +0100

    1 file modified, 1 added

--- index.js
+++ index.js
@@ -1,28 +1,13 @@
 // Tutaj napisz swój kod
 
 const table = document.querySelector(".table-auto");
-const tr = table.querySelectorAll("tr");
-const theadRow = table.querySelector("thead tr");
-const tbodyRows = table.querySelectorAll("tbody tr");
+const trs = table.querySelectorAll("tbody tr");
+const ths = table.querySelectorAll("thead th");
 
-const createWalker = elem =>
-  document.createTreeWalker(elem, NodeFilter.SHOW_ELEMENT);
+function sortBy() {
+  console.log("sortuję");
+}
 
-theadRow.addEventListener("click", e => {
-  if (e.target.localName === "a") {
-    tbodyRows.forEach(tr => {
-      const walker = createWalker(tr);
-
-      while (walker.nextNode()) {
-        const elem = walker.currentNode;
-        const index = elem.cellIndex;
-        console.dir(elem);
-        console.dir(index);
-      }
-    });
-
-    // console.dir(e.target);
-    // console.log(e.target.localName);
-    // console.log(typeof e.target.localName);
-  }
-});
+for (let i = 0; i < ths.length; i++) {
+  ths[i].addEventListener("click", sortBy);
+}
