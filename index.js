940d45065576df0826bbff6a4627aa1ffbcb0370
GrzegorzNecka <strzypior@gmail.com>
2021-01-16 23:39:07 +0100

    1 file modified

--- index.js
+++ index.js
@@ -3,11 +3,19 @@ const table = document.querySelector(".table-auto"),
   trs = table.querySelectorAll("tbody tr"),
   df = document.createDocumentFragment();
 
-function sortBy({ target }) {
-  const theadRow = Array.from(ths);
-  tbodyRows = Array.from(trs);
-  columnIndex = target.cellIndex;
+const deleteNthColor = row => {
+  row.forEach(tr => {
+    if (tr.classList.contains("bg-gray-100")) {
+      tr.classList.remove("bg-gray-100");
+    }
+  });
+};
 
+const addNthColor = newTable => {
+  newTable[0].classList.add("bg-gray-100");
+};
+
+const sortText = (tbodyRows, columnIndex) => {
   tbodyRows.sort(function(a, b) {
     const tdA = a.children[columnIndex].textContent,
       tdB = b.children[columnIndex].textContent;
@@ -20,14 +28,26 @@ function sortBy({ target }) {
       return 0;
     }
   });
+};
+
+//-----------------------
+
+function sortBy({ target }) {
+  const theadRow = Array.from(ths);
+  tbodyRows = Array.from(trs);
+  columnIndex = target.cellIndex;
+
+  deleteNthColor(tbodyRows);
+
+  sortText(tbodyRows, columnIndex);
 
   console.log(tbodyRows);
   tbodyRows.forEach(function(tr) {
     df.appendChild(tr);
   });
 
-  table.querySelector("tbody").appendChild(df);
-  console.log(table);
+  const appendNewLayout = table.querySelector("tbody").appendChild(df);
+  addNthColor(table.querySelectorAll("tbody tr:nth-child(2)"));
 }
 
 for (let i = 0; i < ths.length; i++) {
